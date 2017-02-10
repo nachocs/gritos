af37c9df920db696a60a9f05a0f13e79e80097de
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2017-02-10 10:52:29 +0000

    login by cookie

--- src/js/app/main/header/loginView.js
+++ src/js/app/main/header/loginView.js
@@ -5,6 +5,7 @@ import template from './loginView-t.html';
 import FbView from './fbView';
 import endpoints from '../../util/endpoints';
 import mockup from '../../util/mockups';
+import Cookies from 'js-cookie';
 
 export default Backbone.View.extend({
   template: _.template(template),
@@ -13,6 +14,7 @@ export default Backbone.View.extend({
     this.fbView = new FbView();
     this.fbView.initialize();
     this.listenTo(this.model, 'change', this.render.bind(this));
+    this.checkCookie();
   },
   events: {
     'click #loginSubmit': 'submit',
@@ -33,32 +35,45 @@ export default Backbone.View.extend({
   openMenu() {
     this.$('.login-menu').toggleClass('hidden');
   },
+  checkCookie(){
+    const cookie = Cookies.get('city');
+    const obj = JSON.parse(cookie);
+    this.loginCall(obj);
+  },
+  loginCall(data){
+    const self = this;
+    $.ajax({
+      type: 'POST',
+      url: endpoints.apiUrl + 'login.cgi',
+      data,
+      success(data) {
+        if (data.status !== 'ok') {
+          console.log('error: ', data.status);
+        } else {
+          self.model.set(data.user);
+          Cookies.set('city', {
+            uid: data.uid,
+          });
+        }
+      },
+    });
+  },
   submit(e) {
     e.preventDefault();
-    const self = this;
-    const alias = this.$('#loginAlias').val(), pass = this.$('#loginPassword').val();
-    if ((alias.length < 1) || (pass.length < 1)) {
+    const alias = this.$('#loginAlias').val(),
+      password = this.$('#loginPassword').val();
+    if ((alias.length < 1) || (password.length < 1)) {
       console.log('te olvidaste de poner algo'); // TODO
     } else {
       // mockup
       if (mockup.active){
         const data = mockup.loginMockup;
-        self.model.set(data.user);
+        this.model.set(data.user);
         return;
       } else {
-        $.ajax({
-          type: 'POST',
-          url: endpoints.apiUrl + 'login.cgi',
-          data: {
-            alias,
-            password: pass,
-          },
-          success(data) {
-            if (data.status !== 'ok') {
-              console.log('error: ', data.status);
-            }
-            self.model.set(data.user);
-          },
+        this.loginCall({
+          alias,
+          password,
         });
       }
     }
