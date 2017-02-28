21f94d0f3c75ca8ffd5b89fd8966e66efdb368cd
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2017-02-28 17:11:56 +0000

    case problem

---
+++ src/js/app/main/header/notificacionesCollectionView.js
@@ -0,0 +1,30 @@
+import Backbone from 'backbone';
+import NotificacionesItemView from './notificacionesItemView';
+import NotificacionesCollection from '../../models/notificacionesCollection';
+
+export default Backbone.View.extend({
+  itemView: NotificacionesItemView,
+  tagName: 'ul',
+  className:'mdl-shadow--4dp',
+  initialize(){
+    this.collection = NotificacionesCollection;
+    this.views = {};
+    this.listenTo(NotificacionesCollection, 'add', this.renderOne.bind(this));
+
+  },
+  render() {
+    this.$el.html((this.collection.length < 1) ? 'No hay nuevas notificaciones' : '');
+    this.collection.each(function (model) {
+      this.renderOne(model);
+    }, this);
+    return this;
+  },
+  renderOne(model) {
+    const notificacionesItemView = new this.itemView({
+      model,
+      userModel: this.userModel,
+    });
+    this.views[model.id] = notificacionesItemView;
+    this.$el.prepend(notificacionesItemView.render().el);
+  },
+});
