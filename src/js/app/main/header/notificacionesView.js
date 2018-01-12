28cda6123abc7af6da65accd98fc1ee798577485
Nacho Carmona <pedro.carmona@extern.isban.com>
2018-01-12 13:38:52 -0500

    dreamy para notificaciones

--- src/js/app/main/header/notificacionesView.js
+++ src/js/app/main/header/notificacionesView.js
@@ -8,7 +8,7 @@ import NotificacionesCollection from '../../models/notificacionesCollection';
 import NotificacionesUserModel from '../../models/notificacionesUserModel';
 
 const Model = Backbone.Model.extend({
-  defaults:{
+  defaults: {
     active: false,
     counter: 0,
     show: false,
@@ -20,17 +20,17 @@ export default Backbone.View.extend({
   events: {
     'click': 'toggleNotificaciones',
   },
-  toggleNotificaciones(){
+  toggleNotificaciones() {
     this.model.set('show', !this.model.get('show'));
     this.model.set('counter', 0);
-    if (this.model.get('show')){
-      NotificacionesCollection.forEach((model)=>{
+    if (this.model.get('show')) {
+      NotificacionesCollection.forEach((model) => {
         const data = model.toJSON();
-        if (data.tipo === 'msg'){
+        if (data.tipo === 'msg') {
           const foro = data.indice + '/' + data.entry.ID;
           NotificacionesUserModel.update(data.tipo, foro, data.entry[data.subtipo], data.subtipo);
         }
-        if (data.tipo === 'yo'){
+        if (data.tipo === 'yo') {
           const foro = data.indice;
           NotificacionesUserModel.update(data.tipo, foro, data.entry.ID);
         }
@@ -38,25 +38,25 @@ export default Backbone.View.extend({
       });
     }
   },
-  initialize(){
+  initialize() {
     this.notificacionesCollectionView = new NotificacionesCollectionView();
 
     this.listenTo(this.model, 'change', this.render.bind(this));
-    this.listenTo(userModel, 'change', (user)=>{
+    this.listenTo(userModel, 'change', (user) => {
       this.model.set('active', user.id ? true : false);
     });
-    this.listenTo(NotificacionesCollection, 'add', ()=>{
-      const notRead = NotificacionesCollection.filter((model)=> !model.get('read'));
+    this.listenTo(NotificacionesCollection, 'add', () => {
+      const notRead = NotificacionesCollection.filter((model) => !model.get('read'));
       this.model.set('counter', notRead.length);
     });
-    this.listenTo(NotificacionesCollection, 'remove', ()=>{
+    this.listenTo(NotificacionesCollection, 'remove', () => {
       this.model.set('counter', NotificacionesCollection.length);
     });
-    this.listenTo(this.model, 'change:show', (model)=>{
-      if (model.get('show')){
-        $('body').on('click.toggleNotificaciones', (e)=>{
-          if (!$(e.target).hasClass('notis-icon')){
-            if (this.model.get('show')){
+    this.listenTo(this.model, 'change:show', (model) => {
+      if (model.get('show')) {
+        $('body').on('click.toggleNotificaciones', (e) => {
+          if (!$(e.target).hasClass('notis-icon')) {
+            if (this.model.get('show')) {
               this.model.set('show', false);
               $('body').off('click.toggleNotificaciones');
             }
@@ -65,7 +65,7 @@ export default Backbone.View.extend({
       }
     });
   },
-  render(){
+  render() {
     this.$el.html(this.template(this.serializer()));
     // if (NotificacionesCollection.length>0){
     this.$('.notificaciones-collection-view').html(this.notificacionesCollectionView.render().el);
@@ -73,7 +73,7 @@ export default Backbone.View.extend({
     this.delegateEvents();
     return this;
   },
-  serializer(){
+  serializer() {
     return this.model.toJSON();
   },
 });
