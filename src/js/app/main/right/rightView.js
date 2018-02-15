db76d435f98ea3fcf29c6d9b88518e17b95ab962
Nacho Carmona <pedro.carmona@extern.isban.com>
2018-02-15 10:01:43 -0500

    just to keep it in hand

--- src/js/app/main/right/rightView.js
+++ src/js/app/main/right/rightView.js
@@ -2,6 +2,8 @@ import ViewBase from '../base/ViewBase';
 import template from './rightView.html';
 import _ from 'lodash';
 
+
+//https://gritos.com/jsgritos/api/json.cgi?indice=gritos/avengers&encontrar=IMAGEN0_THUMB&max=1
 export default ViewBase.extend({
   template: _.template(template),
   initialize() {},
