5a6ad5965f2f7aebc5b5c63a80b5ca27ecb01d78
Pedro Ignacio Carmona Sarmiento <nachocarmona@gmail.com>
2018-12-06 13:19:31 -0500

    load secure dreamys

--- src/js/app/util/dreamysService.js
+++ src/js/app/util/dreamysService.js
@@ -14,12 +14,14 @@ const Service = class DreamysService {
     this.personalDreamysLoaded = {};
   }
   fetchGeneralDreamys() {
-    if (this.generalDreamysLoaded) { return; }
+    if (this.generalDreamysLoaded) {
+      return;
+    }
     const url = 'json.cgi?indice=dreamys&encontrar=public';
 
     return Observable
       .ajax(endpoints.apiUrl + url)
-      .map(e => e.response)
+      .map(e => this.mapDreamys(e.response))
       .subscribe((re) => {
         this.generalDreamysLoaded = true;
         this.generalDreamysSubject.next(re);
@@ -28,13 +30,26 @@ const Service = class DreamysService {
   getGeneralDreamys() {
     return this.gDreamys;
   }
+  mapDreamys(l){
+    if (l.IMAGEN1_URL) {
+      l.IMAGEN1_URL = l.IMAGEN1_URL.replace(/^https?\:\/\/dreamers\.com/, '');
+      l.IMAGEN1_URL = l.IMAGEN1_URL.replace(/^\/\/dreamers\.com\/mrdreamy\//, '/mrdreamy/');
+    }
+    if (l.IMAGEN1_THUMB) {
+      l.IMAGEN1_THUMB = l.IMAGEN1_THUMB.replace(/^https?\:\/\/dreamers\.com/, '');
+      l.IMAGEN1_THUMB = l.IMAGEN1_THUMB.replace(/^\/\/dreamers\.com\/mrdreamy\//, '/mrdreamy/');
+    }
+    return l;
+  }
   fetchPersonalDreamys(id) {
-    if (this.personalDreamysLoaded[id]) { return; }
+    if (this.personalDreamysLoaded[id]) {
+      return;
+    }
     const url = 'json.cgi?indice=dreamys&encontrar=ciudadano=' + id;
 
     return Observable
       .ajax(endpoints.apiUrl + url)
-      .map(e => e.response)
+      .map(e => this.mapDreamys(e.response))
       .subscribe((re) => {
         this.personalDreamysLoaded[id] = true;
         this.personalDreamysSubject.next(re);
